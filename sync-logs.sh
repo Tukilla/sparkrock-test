0c18aff519282d20cc11eff1a695263a470045dc
Tukilla <146824386+Tukilla@users.noreply.github.com>
2025-09-04 12:11:43 +0200

    Update sync-logs.sh
    made generic for better usage

--- sync-logs.sh
+++ sync-logs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Sync Nginx logs to S3
-aws s3 sync /home/ec2-user/logs s3://sparkrock-logs-476186718905-eu-north-1/nginx/ --delete
+aws s3 sync ~/logs s3://sparkrock-logs-<account-id>-<region>/nginx/ --delete
+
